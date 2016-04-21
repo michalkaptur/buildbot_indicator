4dfcdb3f852fb3d564a43f1270b6787b87913226
edd44 <kaptur.michal@gmail.com>
2016-04-21 19:16:29 +0200

    POC refactor

--- console.py
+++ console.py
@@ -5,22 +5,45 @@ SERVER_ADDRESS = "http://buildbot.buildbot.net"
 BUILDERS = ["py26-tw0900", "py26-tw1020", "py26-tw1110"]
 BRANCH = "master"
 
-response = requests.get(SERVER_ADDRESS+"/grid?width=1&branch="+BRANCH)
-soup = bs4.BeautifulSoup(response.text, "html.parser")
-builders_html = soup.select('table.Grid tr')
-regression_stat = {}
-for builder in builders_html:
-    if builder.a.get_text() in BUILDERS:
-        result = builder.find(class_="build").a.get_text()
-        regression_stat[builder.a.get_text()] = result
+class Checker:
+    def __init__(self, server_address, builders, branch):
+        self.server_address = server_address
+        self.builders = builders
+        self.branch = branch
+        self.request_address = self.server_address+"/grid?width=1&branch="+self.branch
+        self.builders_status = {}
 
-failed = []
-for builder,status in regression_stat.items():
-    if status != "OK":
-        failed.append(builder)
+    def get_html(self):
+        response = requests.get(self.request_address)
+        soup = bs4.BeautifulSoup(response.text, "html.parser")
+        return soup.select('table.Grid tr')
 
-if len(failed) > 0:
+    def update_builders_status(self):
+        self.builders_status = {}
+        builders_html = self.get_html()
+        for builder in builders_html:
+            if builder.a.get_text() in BUILDERS:
+                result = builder.find(class_="build").a.get_text()
+                self.builders_status[builder.a.get_text()] = result
+
+    def get_nok_builds(self):
+        nok_builds = []
+        for builder,status in self.builders_status.items():
+            if status != "OK":
+                nok_builds.append(builder)
+        return nok_builds
+
+    def all_builds_succedded(self):
+        for builder,status in self.builders_status.items():
+            if status != "OK":
+                return False
+        return True
+
+checker = Checker(SERVER_ADDRESS, BUILDERS, BRANCH)
+checker.update_builders_status()
+
+if checker.all_builds_succedded() is False:
     print("Failed builds:")
-    print(failed)
+    print(checker.get_nok_builds())
 else:
     print("OK")
