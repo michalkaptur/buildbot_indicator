62145b785494ba3839a01fc020fbb15ea2e6c72a
edd44 <kaptur.michal@gmail.com>
2016-04-21 19:32:20 +0200

    open browser works; removed extended config

--- buildbot_indicator.py
+++ buildbot_indicator.py
@@ -1,27 +1,33 @@
 #!/usr/bin/env python2
 
 import signal
+import webbrowser
 
 from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
 from gi.repository import Notify as notify
 
+SERVER_ADDRESS = "http://buildbot.buildbot.net"
+BUILDERS = ["py26-tw0900", "py26-tw1020", "py26-tw1110"]
+BRANCH = "master"
+NOTIFY_ON_STATUS_CHANGE = True
 
-APPINDICATOR_ID = 'myappindicator'
+
+APPINDICATOR_ID = 'buildbot-indicator'
 
 def main():
-    #first: validate config
-    config = {}
-    execfile("config", config)
-    indicator = appindicator.Indicator.new(APPINDICATOR_ID, gtk.STOCK_YES, appindicator.IndicatorCategory.SYSTEM_SERVICES)
+    indicator = appindicator.Indicator.new(APPINDICATOR_ID,
+                                           gtk.STOCK_YES,
+                                           appindicator.IndicatorCategory.SYSTEM_SERVICES)
     indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
-    indicator.set_menu(build_menu(config))
+    indicator.set_menu(build_menu())
     notify.init(APPINDICATOR_ID)
     gtk.main()
 
-def build_menu(config):
+
+def build_menu():
     menu = gtk.Menu()
-    item_branch_name = gtk.MenuItem(config['BRANCH_NAME'])
+    item_branch_name = gtk.MenuItem(BRANCH)
     item_branch_name.set_sensitive(False)
     menu.append(item_branch_name)
     menu.append(gtk.SeparatorMenuItem())
@@ -29,15 +35,18 @@ def build_menu(config):
     item_open_buildbot.connect('activate', open_buildbot)
     menu.append(item_open_buildbot)
     item_quit = gtk.MenuItem('quit')
-    item_quit.connect('activate', quit)
+    item_quit.connect('activate', terminate)
     menu.append(item_quit)
     menu.show_all()
     return menu
 
-def open_buildbot(_):
-    notify.Notification.new("Opening browser", "yeah, open that browser already", None).show()
 
-def quit(_):
+def open_buildbot(_):
+    webbrowser.open_new(SERVER_ADDRESS)
+#    notify.Notification.new("Opening browser", "yeah, open that browser already", None).show()
+
+
+def terminate(_):
     notify.uninit()
     gtk.main_quit()
 
